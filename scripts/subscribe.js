e8f02ce3d23c00f6749fab23a1ed9eec45fb24fc
Marcelo Autran <mrautran@gmail.com>
2023-11-05 18:01:14 -0500

    Success and failure states for email input

--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -1,18 +1,29 @@
+function shakeInput() {
+    const emailInput = document.getElementById("email-input");
+    emailInput.classList.remove("shake")
+    emailInput.classList.add("shake")
+    emailInput.style['text-decoration'] = 'underline';
+    emailInput.style['text-decoration-color'] = 'red';
+}
+
 function subscribe() {
     const emailInput = document.getElementById("email-input");
     const email = emailInput.value;
 
     if (!email.includes('@')) {
-        emailInput.classList.remove("shake")
-        emailInput.classList.add("shake")
-        emailInput.style['text-decoration'] = 'underline';
-        emailInput.style['text-decoration-color'] = 'red';
+        shakeInput();
     } else {
         fetch("https://us-east4-complete-galaxy-404116.cloudfunctions.net/mailing-list-subscribe", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({email: email})
-        });
+        }).then((resp) => { if(resp.ok) {
+            emailInput.value = "";
+            successElement = document.getElementById("success-message");
+            successElement.style['display'] = 'flex';
+        } else {
+            shakeInput();
+        }})
     }
 }
 
